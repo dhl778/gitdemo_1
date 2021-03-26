9a322f010ff022ec7b1f2f0ede91246a6349fa16
dhl778 <dhlai1@163.com>
2021-03-26 22:20:26 +0800

    Default Changelist

---
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -0,0 +1,31 @@
+package com.example.demo;
+
+import com.example.demo.comment.Discuss;
+import com.example.demo.repository.DiscussRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+    @Autowired
+    private DiscussRepository repository;
+    @Test
+    public void     selectCommentByKeys() {
+        List<Discuss> list =repository.findByAuthorNotNull();
+        System.out.println(list);
+    }
+
+
+
+
+
+
+
+}
